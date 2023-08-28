65323db2418f3d57b9fad8a0b381a6ee5cb01c5d
Swarzstein <zorrillagja@gmail.com>
2023-08-28 13:03:38 -0400

    forms working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
   has_many :likes
+  has_many :comments
+
+  after_save :post_counter_update
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
+  def post_counter_update
+    author.increment!(:posts_counter)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_202843) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_144027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
     t.string "text"
+    t.bigint "user_id"
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_202843) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -34,11 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_202843) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "author_id", null: false
     t.string "title"
     t.string "text"
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_202843) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "comments", "posts"

11012ddcb5482db4354e5ce7539a25a4baa0892a
Swarzstein <zorrillagja@gmail.com>
2023-09-01 15:36:47 -0400

    aditional test required for the posts index spec added

--- spec/integration/posts_index_spec.rb
+++ spec/integration/posts_index_spec.rb
@@ -62,14 +62,38 @@ describe 'Posts Index', type: :system do
     )
   end
 
+  it 'displays author\'s info' do
+    visit user_posts_path(author)
+    expect(page).to have_content('Akai321')
+    expect(page).to have_css("img[src*='https://Akaiiii.jpg']")
+  end
+
   it 'displays all posts' do
     visit user_posts_path(author)
     expect(page).to have_content('The way of the Water')
     expect(page).to have_content('The way of the Fire')
+    expect(page).to have_content('Honor in martial arts')
     expect(page).to have_content('In martial arts, the way of the water is the way of the soft and yielding.')
     expect(page).to have_content('I prefer the way of the leaf, which is the way of the ninja.')
+    expect(page).to have_content('Pagination')
+  end
+
+  it 'displays the number of comments each post has' do
+    visit user_posts_path(author)
     expect(page).to have_content('Comments: 3')
+    expect(page).to have_content('Comments: 2')
     expect(page).to have_content('Comments: 0')
+  end
+
+  it 'displays the comments for each post' do
+    visit user_posts_path(author)
+    expect(page).to have_content('I prefer the way of the leaf, which is the way of the ninja.')
+    expect(page).to have_content('The way of the leaf is the way of the coward.')
+    expect(page).to have_content('The way of the leaf is not the way of the coward,' \
+                                  'ninjas are not cowards ' \
+                                  'but warriors who fight in the shadows.')
+    expect(page).to have_content('I resonate with the way of the fire.')
+    expect(page).to have_content('finally we agree on something.')
   end
 
   scenario "When I click a post's title, it redirects me to that post's show page" do
